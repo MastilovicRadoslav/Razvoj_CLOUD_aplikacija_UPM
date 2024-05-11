fc441411cfe5eba107f660a216f43e782a5037f7
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-05-11 15:33:42 +0200

    HomeController

--- Reddit/WebRole/Controllers/HomeController.cs
+++ Reddit/WebRole/Controllers/HomeController.cs
@@ -1,12 +1,49 @@
-﻿using System.Web.Mvc;
+﻿using FrontReddit.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Web.Mvc;
 
 namespace WebRole.Controllers
 {
 	public class HomeController : Controller
 	{
-		public ActionResult Index()
-		{
-			return View();
-		}
+        // GET: Home page
+        // Prikaz početne stranice
+        public ActionResult Index()
+        {
+            List<Post> posts = new List<Post>();
+
+            Post p1 = new Post();
+            p1.Title = "Naslov posta 1";
+            p1.Description = "Neki kratak opis 1";
+            p1.Image = "/Images/Backgrounds/profile.jpg";
+            p1.Comments.Add(new Comment("Neki komentar 1", "neko2@gmail.com"));
+            p1.UserEmail = "neko@gmail.com";
+            p1.Like = 0;
+            p1.UnLike = 0;
+            posts.Add(p1);
+
+            Post p2 = new Post();
+            p2.Title = "Naslov posta 2";
+            p2.Description = "Neki kratak opis 2";
+            p2.Image = "";
+            p2.Comments.Add(new Comment("Neki komentar 2", "neko2@gmail.com"));
+            p2.UserEmail = "neko@gmail.com";
+            p2.Like = 0;
+            p2.UnLike = 0;
+            posts.Add(p2);
+
+            Post p3 = new Post();
+            p3.Title = "Naslov posta 3";
+            p3.Description = "Neki kratak opis 3";
+            p3.Image = "";
+            p3.Comments.Add(new Comment("Neki komentar 3", "neko2@gmail.com"));
+            p3.UserEmail = "neko@gmail.com";
+            p3.Like = 0;
+            p3.UnLike = 0;
+            posts.Add(p3);
+
+            return View(posts);
+        }
 	}
 }
