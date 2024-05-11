96804f2d9807742a3bdaed29f748908edca7114d
dovla2001 <vmandic83@gmail.com>
2024-05-11 15:17:14 +0200

    LogInController

---
+++ Reddit/WebRole/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using FrontReddit.Models;
+using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace FrontReddit.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        // Prikaz za prijavu
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Login
+        // Obrada prijave
+        [System.Web.Mvc.HttpPost]
+        public ActionResult SubmitLogin()
+        {
+            var email = Request["email"] ?? string.Empty;
+            var password = Request["password"] ?? string.Empty;
+
+            User user = AppContext.Users.Find(u => u.Email == email && u.Password == password);
+            if (user != null)
+            {
+                Session["LoggedInUser"] = user;
+
+                // Ispis primljenih podataka od klijenta
+                Debug.WriteLine("Email adresa: " + email);
+                Debug.WriteLine("Lozina: " + password);
+                Debug.WriteLine("Uspešna prijava!");
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Ispis primljenih podataka od klijenta
+            Debug.WriteLine("Email: " + email);
+            Debug.WriteLine("Lozina: " + password);
+            Debug.WriteLine("Prijava je neuspešna!");
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
