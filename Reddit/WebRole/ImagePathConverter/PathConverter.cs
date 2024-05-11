c38ed028575b4a203769ad3a315e71bb3ffe2a30
dovla2001 <vmandic83@gmail.com>
2024-05-11 15:16:14 +0200

    PathConverter

---
+++ Reddit/WebRole/ImagePathConverter/PathConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace FrontReddit.ImagePathConverter
+{
+    public class PathConverter
+    {
+        public string ReplacePath(string path)
+        {
+            string fileName = Path.GetFileName(path);
+            string newFormat = $"/Images/Profile/{fileName}";
+            return newFormat;
+        }
+    }
+}
