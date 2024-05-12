5dd79a3c0169ccc835aebc2c540a3ee06582cbdc
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-05-12 12:28:26 +0200

    Models

--- Reddit/WebRole/Models/AppContext.cs
+++ Reddit/WebRole/Models/AppContext.cs
@@ -5,5 +5,6 @@ namespace Models
     public static class AppContext
     {
         public static List<User> Users { get; set; } = new List<User>();
+        public static HomePagePostLists homePagePostLists = new HomePagePostLists();
     }
 }
---
+++ Reddit/WebRole/Models/HomePagePostLists.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Models
+{
+    public class HomePagePostLists
+    {
+        public List<Post> MyPosts { get; set; } = new List<Post>();
+        public List<Post> AllPosts { get; set; } = new List<Post>();
+    }
+}
