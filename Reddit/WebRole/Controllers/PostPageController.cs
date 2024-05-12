00fd1d29409f02024339089474749e7798cd415e
MastilovicRadoslav <radoslav930@gmail.com>
2024-05-12 13:10:25 +0200

    PostPageController

---
+++ Reddit/WebRole/Controllers/PostPageController.cs
@@ -0,0 +1,21 @@
+﻿using Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace WebRole.Controllers
+{
+    public class PostPageController : Controller
+    {
+        // GET: PostPage
+        // Prikaz odabranog posta na osnovu naslova
+        public ActionResult PostPage(string postTitle)
+        {
+            Post post = AppContext.homePagePostLists.AllPosts.FirstOrDefault(p => p.Title == postTitle);
+            if(post != null)
+            {
+                return View(post);
+            }
+            return View();
+        }
+    }
+}
