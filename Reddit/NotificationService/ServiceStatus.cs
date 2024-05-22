9cd6a9163cde076e1639ec8ef61dac59adeabeb6
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-05-22 18:09:59 +0200

    NotificationService

---
+++ Reddit/NotificationService/ServiceStatus.cs
@@ -0,0 +1,12 @@
+﻿using Common.Interfaes;
+
+namespace NotificationService
+{
+    public class ServiceStatus : ICheckServiceStatus
+    {
+        public bool CheckServiceStatus()
+        {
+            return true;
+        }
+    }
+}
