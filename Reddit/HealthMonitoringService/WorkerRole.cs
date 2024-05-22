0d66b2df4b9b0af3de684c0c255d3469ed5b635d
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-05-22 17:48:58 +0200

    HealthMonitoringService

---
+++ Reddit/HealthMonitoringService/WorkerRole.cs
@@ -0,0 +1,190 @@
+using Common.Interfaes;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Polly;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.ServiceModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthMonitoringService
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
+        private ICheckServiceStatus serviceRedditProxy;
+        private ICheckServiceStatus serviceNotificationProxy;
+
+        public override void Run()
+        {
+            Trace.TraceInformation("HealthMonitoringService is running");
+
+            try
+            {
+                this.RunWithRetryAsync(this.cancellationTokenSource.Token).Wait();
+            }
+            finally
+            {
+                this.runCompleteEvent.Set();
+            }
+        }
+
+        private async Task RunWithRetryAsync(CancellationToken token)
+        {
+
+            var retryPolicy = Policy
+                .Handle<CommunicationException>()
+                .Or<TimeoutException>()
+                .WaitAndRetryAsync(
+                    retryCount: 3,
+                    sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    onRetry: (exception, timeSpan, retryCount, context) =>
+                    {
+                        Trace.WriteLine($"Retry {retryCount} encountered a {exception.GetType().Name}. Waiting {timeSpan} before next retry. Exception: {exception.Message}");
+                    });
+
+
+            await retryPolicy.ExecuteAsync(async () =>
+            {
+                await RunAsync(token);
+            });
+        }
+
+        private async Task MonitorWebRoleStatusAsync()
+        {
+            /*bool serviceRedditAvailable = serviceRedditProxy.CheckServiceStatus();
+            if (serviceRedditAvailable)
+            {
+                Trace.WriteLine("RedditService is running!");
+                // ste = new StatusTableEntry("PortfolioStatus","OK");
+                // hms.InsertPortfolioStatus(ste);
+            }*/
+            bool notificationServicaAvailable = serviceNotificationProxy.CheckServiceStatus();
+            if (notificationServicaAvailable)
+            {
+                Trace.WriteLine("NotificationService is running!");
+                //ste = new StatusTableEntry("NotificationStatus", "OK");
+                //hms.InsertNotificationStatus(ste);
+            }
+
+            await Task.Delay(5000);
+        }
+
+        public override bool OnStart()
+        {
+            // Use TLS 1.2 for Service Bus connections
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            // Set the maximum number of concurrent connections
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // For information on handling configuration changes
+            // see the MSDN topic at https://go.microsoft.com/fwlink/?LinkId=166357.
+
+            //ConnecToReddit();
+            ConnecToNotificationService();
+
+            bool result = base.OnStart();
+
+            Trace.TraceInformation("HealthMonitoringService has been started");
+
+            return result;
+        }
+
+        public override void OnStop()
+        {
+            Trace.TraceInformation("HealthMonitoringService is stopping");
+
+            this.cancellationTokenSource.Cancel();
+            this.runCompleteEvent.WaitOne();
+
+            base.OnStop();
+
+            Trace.TraceInformation("HealthMonitoringService has stopped");
+        }
+
+        private async Task RunAsync(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                try
+                {
+                    EnsureConnection();
+
+                    await MonitorWebRoleStatusAsync();
+                }
+                catch (CommunicationException ex)
+                {
+                    Trace.WriteLine($"CommunicationException: {ex.Message}");
+                    throw;
+                }
+                catch (TimeoutException ex)
+                {
+                    Trace.WriteLine($"TimeoutException: {ex.Message}");
+                    throw;
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine($"Unexpected exception: {ex.Message}");
+                    throw;
+                }
+            }
+        }
+
+        private void EnsureConnection()
+        {
+            if (serviceRedditProxy == null)
+            {
+                //ConnecToReddit();
+            }
+            try
+            {
+                //serviceRedditProxy.CheckServiceStatus();
+            }
+            catch (CommunicationException ex)
+            {
+                Trace.WriteLine($"Reddit is down! {ex} \n Reconnecting...");
+                //ste = new StatusTableEntry("PortfolioStatus", "NOT_OK");
+                // hms.InsertPortfolioStatus(ste);
+                ConnecToReddit();
+            }
+
+            if (serviceNotificationProxy == null)
+            {
+                ConnecToNotificationService();
+            }
+            try
+            {
+                serviceNotificationProxy.CheckServiceStatus();
+            }
+            catch (CommunicationException ex)
+            {
+                Trace.WriteLine($"Notification is down! {ex} \n Reconnecting...");
+                // ste = new StatusTableEntry("NotificationStatus", "NOT_OK");
+                // hms.InsertNotificationStatus(ste);
+                ConnecToNotificationService();
+            }
+        }
+
+        public void ConnecToReddit()
+        {
+            var endpoint = RoleEnvironment.Roles["RedditService"].Instances[0].InstanceEndpoints[""];
+            var address = new EndpointAddress($"net.tcp://{endpoint.IPEndpoint}/Service");
+            var binding = new NetTcpBinding();
+            ChannelFactory<ICheckServiceStatus> factory = new ChannelFactory<ICheckServiceStatus>(binding, address);
+            serviceRedditProxy = factory.CreateChannel();
+
+        }
+
+        public void ConnecToNotificationService()
+        {
+            var binding = new NetTcpBinding();
+            var endpoint = RoleEnvironment.Roles["NotificationService"].Instances[0].InstanceEndpoints["HealthCheck"];
+            var address = new EndpointAddress($"net.tcp://{endpoint.IPEndpoint.Address}:{endpoint.IPEndpoint.Port}/Service");
+            ChannelFactory<ICheckServiceStatus> factory = new ChannelFactory<ICheckServiceStatus>(binding, address);
+            serviceNotificationProxy = factory.CreateChannel();
+        }
+    }
+}
