716317a9b71bab83e066289c2c391f96857dc4e5
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-05-22 17:37:24 +0200

    Common

---
+++ Reddit/Common/Interfaces/ICheckServiceStatus.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+
+namespace Common.Interfaes
+{
+    [ServiceContract]
+    public interface ICheckServiceStatus
+    {
+        [OperationContract]
+        bool CheckServiceStatus();
+    }
+}
