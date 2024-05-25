8030cb750d3798fd00e8406ec119284af8c0eee5
ResProjekat <medic.pr101.2019@uns.ac.rs>
2024-05-25 20:07:25 +0200

    Entities and HealthStatusRepository

---
+++ Reddit/Common/Entities/HealthStatus.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace Common.Entities
+{
+    public class HealthStatus : TableEntity
+    {
+        public string ServiceType { get; set; }
+        public string Status { get; set; }
+
+        private static long _redditCounter = 0;
+        private static readonly object _redditLock = new object();
+
+        private static long _notificationCounter = 0;
+        private static readonly object _notificationLock = new object();
+
+        public HealthStatus(string serviceType)
+        {
+            PartitionKey = "HealthStatus";
+            RowKey = $"{serviceType}-{GenerateNextId(serviceType)}";
+            ServiceType = serviceType;
+        }
+
+        public HealthStatus() { }
+
+        public HealthStatus(string serviceType, string status)
+        {
+            PartitionKey = "HealthStatus";
+            RowKey = $"{serviceType}-{GenerateNextId(serviceType)}";
+            ServiceType = serviceType;
+            Status = status;
+        }
+
+        private static long GenerateNextId(string serviceType)
+        {
+            if (serviceType.Equals("RedditService", StringComparison.OrdinalIgnoreCase))
+            {
+                lock (_redditLock)
+                {
+                    return _redditCounter++;
+                }
+            }
+            else if (serviceType.Equals("NotificationService", StringComparison.OrdinalIgnoreCase))
+            {
+                lock (_notificationLock)
+                {
+                    return _notificationCounter++;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Unknown service type");
+            }
+        }
+    }
+}
---
+++ Reddit/Common/Repositories/HealthStatusRepository.cs
@@ -0,0 +1,49 @@
+﻿using Common.Entities;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace Common.Repositories
+{
+    public class HealthStatusRepository
+    {
+        private readonly CloudStorageAccount _storageAccount;
+        private readonly CloudTable _table;
+
+        public HealthStatusRepository()
+        {
+            string connectionString = CloudConfigurationManager.GetSetting("HealthStatusConnectionString");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Connection string for Azure Storage is not set.");
+            }
+
+            _storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("HealthStatusConnectionString"));
+            CloudTableClient tableClient = new CloudTableClient(new Uri(_storageAccount.TableEndpoint.AbsoluteUri), _storageAccount.Credentials);
+            _table = tableClient.GetTableReference("HealthStatusTableTemp");
+            _table.CreateIfNotExists();
+        }
+
+        public void Create(HealthStatus newHealthStatus)
+        {
+            try
+            {
+                TableOperation insertOperation = TableOperation.Insert(newHealthStatus);
+                TableResult result = _table.Execute(insertOperation);
+
+                if (result.HttpStatusCode < 200 || result.HttpStatusCode >= 300)
+                {
+                    throw new InvalidOperationException("Failed to insert user into Azure Table Storage.");
+                }
+
+                //Debug.WriteLine($"Insert operation HTTP status code: {result.HttpStatusCode}");
+            }
+            catch (Exception ex)
+            {
+                //Debug.WriteLine($"Exception during insert operation: {ex.Message}");
+                throw new Exception($"{ex}");
+            }
+        }
+    }
+}
