d79a9a53a576041ee1578f37281b919a97c43345
ResProjekat <medic.pr101.2019@uns.ac.rs>
2024-05-28 20:48:23 +0200

    Model classes

---
+++ Reddit/Common/Entities/CommentData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Common.Entities
+{
+    public class CommentData : TableEntity
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int PostId { get; set; }
+        public string UserEmail { get; set; }
+
+        private static long _idCounter = 0;
+        private static readonly object _idLock = new object();
+
+        public CommentData(string text)
+        {
+            PartitionKey = "Comment";
+            RowKey = GenerateNextId().ToString();
+        }
+
+        public CommentData() { }
+
+        public CommentData(string text, int postId, string userEail)
+        {
+            Id = (int)GenerateNextId();
+            Text = text;
+            PostId = postId;
+            UserEmail = userEail;
+        }
+
+        private static long GenerateNextId()
+        {
+            lock (_idLock)
+            {
+                return _idCounter++;
+            }
+        }
+    }
+}
---
+++ Reddit/Common/Entities/Favourites.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Common.Entities
+{
+    public class Favourites : TableEntity
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string UserEmail { get; set; }
+
+        private static long _idCounter = 0;
+        private static readonly object _idLock = new object();
+
+        public Favourites(int postId)
+        {
+            PartitionKey = "Favourites";
+            RowKey = GenerateNextId().ToString();
+        }
+
+        public Favourites() { }
+
+        public Favourites(int postId, string userEmail)
+        {
+            Id = (int)GenerateNextId();
+            PostId = postId;
+            UserEmail = userEmail;
+        }
+
+        private static long GenerateNextId()
+        {
+            lock (_idLock)
+            {
+                return _idCounter++;
+            }
+        }
+    }
+}
