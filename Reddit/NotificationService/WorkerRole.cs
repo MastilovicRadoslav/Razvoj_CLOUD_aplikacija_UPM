0a4991e33c6b134874816675b79ea862006281f2
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-05-29 18:19:05 +0200

    NotificationService

--- Reddit/NotificationService/WorkerRole.cs
+++ Reddit/NotificationService/WorkerRole.cs
@@ -1,9 +1,16 @@
+using Common.Comment_queue;
+using Common.Entities;
 using Common.Interfaes;
 using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage.Queue;
+using NotificationService.Notification_via_email;
+using RedditService;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using System.ServiceModel;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,6 +22,8 @@ namespace NotificationService
         private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
         ServiceHost serviceHost;
         private readonly string internalEndpointName = "HealthCheck";
+        private readonly FavouritesServiceProvider favouritesService = new FavouritesServiceProvider();
+        private readonly EmailSender emailSender = new EmailSender();
 
         public override void Run()
         {
@@ -23,6 +32,10 @@ namespace NotificationService
             try
             {
                 this.RunAsync(this.cancellationTokenSource.Token).Wait();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
             }
             finally
             {
@@ -82,6 +95,55 @@ namespace NotificationService
             {
                 Trace.TraceInformation("Working");
                 await Task.Delay(1000);
+
+                CloudQueue queue = QueueHelper.GetQueueReference("comments");
+                while (true)
+                {
+                    CloudQueueMessage message = queue.GetMessage();
+                    if (message == null)
+                    {
+                        Trace.TraceInformation("Trenutno ne postoji poruka u redu.");
+                    }
+                    else
+                    {
+                        Trace.TraceInformation(String.Format("Poruka glasi: {0}", message.AsString));
+
+                        List<string> emails = new List<string>();
+                        CommentData commentData = CommentData.FromString(message.AsString);
+                        List<Favourites> allFavouritesFromTable = favouritesService.GetAllFavourites();
+                        foreach (var favourites in allFavouritesFromTable)
+                        {
+                            if (favourites.PostId == commentData.PostId)
+                            {
+                                emails.Add(favourites.UserEmail);
+                            }
+                        }
+
+                        if (emails.Count != 0)
+                        {
+                            string subject = "Postavljen je novi komentar na temu na koju ste pretplaæeni";
+                            StringBuilder bodyBuilder = new StringBuilder();
+                            foreach (var email in emails)
+                            {
+                                bodyBuilder.AppendLine(email);
+                            }
+                            bodyBuilder.AppendLine();
+                            bodyBuilder.AppendLine("Text komentara koji je postavljen: " + commentData.Text);
+                            string body = bodyBuilder.ToString().TrimEnd();
+                            body = body.Replace(Environment.NewLine, "<br/>");
+                            emailSender.SendEmail("drsprojekat2023@gmail.com", subject, body);
+                        }
+
+                        Trace.TraceInformation("Informacije o poslatoj grupi notifikacija: ");
+                        Trace.TraceInformation("|Datum i vreme: " + DateTime.Now + "|ID komentara: " + commentData.Id + "|Broj poslatih mejlova: " + emails.Count + "|");
+
+                        if (message.DequeueCount >= 2)
+                        {
+                            queue.DeleteMessage(message);
+                        }
+                    }
+                    Thread.Sleep(5000);
+                }
             }
         }
     }
