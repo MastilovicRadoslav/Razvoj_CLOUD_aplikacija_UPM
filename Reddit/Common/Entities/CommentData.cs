17ed2a603fbbdc6050dccffcbfeb845b811d7c2b
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-05-29 18:10:36 +0200

    Common

--- Reddit/Common/Entities/CommentData.cs
+++ Reddit/Common/Entities/CommentData.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
 
 namespace Common.Entities
 {
@@ -34,6 +35,34 @@ namespace Common.Entities
             {
                 return _idCounter++;
             }
+        }
+
+        public override string ToString()
+        {
+            return $"Komentar: ID komentara: {Id}, Text: {Text}, ID posta: {PostId}, Email korisnika koji je postavio komentar: {UserEmail}";
+        }
+
+        public static CommentData FromString(string message)
+        {
+            var parts = message.Split(new[] { ", " }, StringSplitOptions.None);
+            var idPart = parts[0].Split(new[] { ": " }, StringSplitOptions.None)[2];
+            var textPart = parts[1].Split(new[] { ": " }, StringSplitOptions.None)[1];
+            var postIdPart = parts[2].Split(new[] { ": " }, StringSplitOptions.None)[1];
+            var emailPart = parts[3].Split(new[] { ": " }, StringSplitOptions.None)[1];
+
+            int id = int.Parse(idPart);
+            string text = textPart;
+            int postId = int.Parse(postIdPart);
+            string userEmail = emailPart;
+
+            var commentData = new CommentData(text, postId, userEmail)
+            {
+                Id = id,
+                Text = text,
+                PostId = postId,
+                UserEmail = userEmail
+            };
+            return commentData;
         }
     }
 }
